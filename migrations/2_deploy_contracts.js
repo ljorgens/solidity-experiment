640e25aac5596f53210100ce2f1b736a6cc836ee
ljorgens <leejorgensen22@gmail.com>
2017-09-20 09:26:00 -0700

    initial

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,14 @@
+var ConvertLib = artifacts.require("./ConvertLib.sol");
+var MetaCoin = artifacts.require("./MetaCoin.sol");
+var ProofOfExistence1 = artifacts.require("./ProofOfExistence1.sol");
+var ProofOfExistence2 = artifacts.require("./ProofOfExistence2.sol");
+var ProofOfExistence3 = artifacts.require("./ProofOfExistence3.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(ConvertLib);
+  deployer.link(ConvertLib, MetaCoin);
+  deployer.deploy(MetaCoin);
+  deployer.deploy(ProofOfExistence1);
+  deployer.deploy(ProofOfExistence2);
+  deployer.deploy(ProofOfExistence3);
+};
